4a797fc2322cf7bea806c40ccf6e62dd551579a9
DanilWork <gnilidanil@gmail.com>
2023-01-13 09:39:09 +0600

    доделал

--- app/src/main/java/com/example/rickandmortyapi/base/BaseFragment.kt
+++ app/src/main/java/com/example/rickandmortyapi/base/BaseFragment.kt
@@ -7,11 +7,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseFragment<VB: ViewBinding, VM:ViewModel>(@LayoutRes layoutId: Int)
-    : Fragment(layoutId) {
+abstract class BaseFragment<VB : ViewBinding, VM : ViewModel>(@LayoutRes layoutId: Int) :
+    Fragment(layoutId) {
 
-    protected abstract val binding:VB
-    protected abstract val viewModel:VM
+    protected abstract val binding: VB
+    protected abstract val viewModel: VM
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/rickandmortyapi/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapi/ui/activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.rickandmortyapi.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.geektech.rickandmorty.R
 
 class MainActivity : AppCompatActivity() {
